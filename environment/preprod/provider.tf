894bf1fa7751ef77cbde4838c6ab6b0ff649fe76
adarshshrivastava2k15-blip <adarsh.shrivastava2k15@gmail.com>
2026-08-07 22:14:29 +0530

    add backend block

--- environment/preprod/provider.tf
+++ environment/preprod/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~>4.0"
     }
   }
+    backend "azurerm" {
+    resource_group_name  = "rg-adarsh"
+    storage_account_name = "terraformstatefilesave"
+    container_name       = "adarshcontainertfstate"
+    key                  = "tfstate"
+  }
 }
 provider "azurerm" {
   features {}
